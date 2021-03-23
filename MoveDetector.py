4b933d4734239ffc99a3e913faef0e3da85454dc
czyjtu <maciejczyjtu@gmail.com>
2021-03-23 12:50:57 +0100

    Improved class suite and added smaple video

--- MoveDetector.py
+++ MoveDetector.py
@@ -8,14 +8,16 @@ def __cut_and_resize(frame, V1: (), V2: ()):
 
     return grey_frame
 
-
+# TODO: type checking
 class MoveDetector:
-    """
-
-    """
     def __init__(self, source):
         self.src = source
         self.capture = None
+        self.ROI = None
+        self.area_threshold = None
+        self.movement_threshold = None
+        self.kernel_blurr_size = None
+        self.show_bounding_box = None
   
 
     def load_source(self, source = None):
@@ -29,13 +31,40 @@ class MoveDetector:
 
         self.capture = capture
 
-
-    def detect(self, treshold):
+    def generate(self, ROI = None, area_threshold = 100, movement_threshold = 500, kernel_blurr_size = (5, 5), show_bounding_box = False, frame_size = None):
         if self.capture is None:
             raise ValueError("you have to load source first.")
 
-        detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=500)
+        self.frame_size = None # TODO: resize frame to the given size
+        self.ROI = ROI
+        self.area_threshold = area_threshold
+        self.movement_threshold = movement_threshold
+        self.kernel_blurr_size = kernel_blurr_size
+        self.show_bounding_box = show_bounding_box
 
+        # TODO: create constant framerate; preferably the video framerate
+        for resized_frame, grey_roi, mask, blurred_mask, final_mask in self.__detect():
+            # TODO: make output postprocessing to the gui wanted format; notice that DEBUG mode will be resolved here
+            cv2.imshow('Frame', resized_frame)
+            cv2.imshow("grey_frame", grey_roi)
+            cv2.imshow('Mask', mask)
+            cv2.imshow("blurred_mask", blurred_mask)
+            cv2.imshow("final", final_mask)
+            print("running")
+
+            k = cv2.waitKey(1) & 0xff
+            if k == 27:
+                break
+        
+        capture.release()
+        cv2.destroyAllWindows()
+    
+        
+    def __detect(self):
+        if self.capture is None:
+            raise ValueError("you have to load source first.")
+
+        detector = cv2.createBackgroundSubtractorKNN(detectShadows=True)
         frameCount = 0
 
         while True:
@@ -44,46 +73,49 @@ class MoveDetector:
                 break
 
             frameCount += 1
-            # Resize the frame
-            resizedFrame = cv2.resize(frame, (1000, 800))
-            roi = resizedFrame[300:800, 600:900]
 
-            # Get the foreground mask
-            fgmask = detector.apply(roi)
+            # TODO: make function/method for frame preprocessing (need to return 3 frames-resized, roi, roi in grayscale)
+            # preprocessing
+            resized_frame = cv2.resize(frame, (800, 500)) #change size
+            # cut area of interest
+            if self.ROI is None:
+                roi = resized_frame[:, :] 
+            else:
+                roi = resized_frame[self.ROI[0][0]: self.ROI[0][1], self.ROI[1][0]: self.ROI[1][1]]
 
-            # removing shadows
-            # _, fgmask = cv2.threshold(fgmask, 254, 255, cv2.THRESH_BINARY)
+            grey_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY) # change to grayscale
 
-            contours, _ = cv2.findContours(fgmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            # TODO: same thing for mask processing
+            mask = detector.apply(grey_roi) 
+            # denoising 
+            blurred_mask = cv2.GaussianBlur(mask, self.kernel_blurr_size, 0) #simple denoising using Gaussian Blurring
+            # blurred_mask = cv2.fastNlMeansDenoising(mask, h=30) # lepsze ale mega wolne
+            _, final_mask = cv2.threshold(blurred_mask,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU) # setting the best treshold automatically
+            # _, final_mask = cv2.threshold(blurred_mask, 200, 255, cv2.THRESH_BINARY) # manual tresholding
+
+            # bounding boxes
+            contours, _ = cv2.findContours(final_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
             for cnt in contours:
                 area = cv2.contourArea(cnt)
-                if area > 100:
-                    x, y, w, h, = cv2.boundingRect(cnt)
-                    cv2.rectangle(roi, (x, y), (x + w, y + h), (0, 255, 0), 3)
+                if area > self.area_threshold:
+                    if self.show_bounding_box:
+                        x, y, w, h, = cv2.boundingRect(cnt)
+                        cv2.rectangle(roi, (x, y), (x + w, y + h), (0, 255, 0), 3)
+                    else :
+                        cv2.drawContours(resized_frame, cnt, -1, (0,255,0), 3)
 
-            count = np.count_nonzero(fgmask)
-            print('Frame: %d, Pixel Count: %d' % (frameCount, count))
-            if (frameCount > 1 and count > treshold):
+            count = np.count_nonzero(final_mask,)
+
+            if (frameCount > 1 and count > self.movement_threshold):
                 print('Movement')
-                cv2.putText(resizedFrame, 'Movement', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+                cv2.putText(resized_frame, 'Movement', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
-            cv2.imshow('Frame', resizedFrame)
-            cv2.imshow('Mask', fgmask)
-    
-            k = cv2.waitKey(1) & 0xff
-            if k == 27:
-                break
+            yield resized_frame, grey_roi, mask, blurred_mask, final_mask
 
-        capture.release()
-        cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
-    det = MoveDetector("sample2.mp4")
+    det = MoveDetector("sample.mp4")
     det.load_source()
-    det.detect(500)
-
-
-
-
+    det.generate(show_bounding_box=True, ROI=((250, 500), (400, 800)))
